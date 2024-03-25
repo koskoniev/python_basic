efe54ae72cd6413c170ca0edb770ead44a70b3da
koskoniev <koskoniev@gmail.com>
2024-03-25 23:58:12 +0200

    dz13. letters range

---
+++ DZ/dz13_letters_range.py
@@ -0,0 +1,6 @@
+import string
+
+range_letters = input("Input range (a-Z): ")
+
+for i in range(string.ascii_letters.find(range_letters[0]), string.ascii_letters.find(range_letters[2]) + 1):
+    print(string.ascii_letters[i], end='')

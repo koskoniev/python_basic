45f55de02b17867a4e8a52b9947862c3e1b74ee6
koskoniev <koskoniev@gmail.com>
2024-03-26 15:53:56 +0200

    dz13 var2

--- DZ/dz13_letters_range.py
+++ DZ/dz13_letters_range.py
@@ -8,5 +8,5 @@ for i in range(string.ascii_letters.find(range_letters[0]), string.ascii_letters
     print(string.ascii_letters[i], end='')
 
 # variant 2
-print()
-print(string.ascii_letters[string.ascii_letters.find(range_letters[0]):string.ascii_letters.find(range_letters[2]) + 1])
+# print()
+# print(string.ascii_letters[string.ascii_letters.find(range_letters[0]):string.ascii_letters.find(range_letters[2]) + 1])

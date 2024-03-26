47c2e3f571bf3a86cf84799367ff155f5b606ae3
koskoniev <koskoniev@gmail.com>
2024-03-26 02:09:01 +0200

    l6 add dz files

--- DZ/dz13_letters_range.py
+++ DZ/dz13_letters_range.py
@@ -1,6 +1,6 @@
 import string
 
-range_letters = input("Input range (a-Z): ")
+range_letters = input("Input range (a-z): ")
 
 for i in range(string.ascii_letters.find(range_letters[0]), string.ascii_letters.find(range_letters[2]) + 1):
     print(string.ascii_letters[i], end='')

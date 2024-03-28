f4f5b0a3428e37c6f11ba112d55f6da4b83c9f50
koskoniev <koskoniev@gmail.com>
2024-03-28 23:44:16 +0200

    Lesson7. dz18

--- DZ/dz18_substring_search.py
+++ DZ/dz18_substring_search.py
@@ -1,8 +1,12 @@
 def second_index(text, some_str):
-    text.find(some_str)
+    si = text.find(some_str) + 1
+    if text.find(some_str, si) >= 0:
+        return text.find(some_str, si)
+    if si == -1:
+        pass
 
 assert second_index("sims", "s") == 3, 'Test1'
-# assert second_index("find the river", "e") == 12, 'Test2'
-# assert second_index("hi", "h") is None, 'Test3'
-# assert second_index("Hello, hello", "lo") == 10, 'Test4'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
 print('ОК')
